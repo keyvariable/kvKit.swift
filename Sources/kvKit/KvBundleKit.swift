c664b7842e52960328b7abd1e0cde8dcf917713d
Svyatoslav Popov <sdpopov@keyvar.com>
2021-02-02 19:19:56 +0300

    KvBundleKit with utilities for the info dictionary and NIBs.

--- Sources/kvKit/KvBundleKit.swift
+++ Sources/kvKit/KvBundleKit.swift
@@ -1,8 +1,24 @@
+//===----------------------------------------------------------------------===//
 //
-//  KvIbKit.swift
+//  Copyright (c) 2021 Svyatoslav Popov.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+//  the License. You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//  specific language governing permissions and limitations under the License.
+//
+//  SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+//
+//  KvBundleKit.swift
 //  kvKit
 //
-//  Created by sdpopov on 13.01.2021.
+//  Created by Svyatoslav Popov on 02.02.2021.
 //
 
 #if os(macOS)
@@ -13,18 +29,44 @@ import UIKit
 
 
 
-/// Collection of the Interface Builder auxiliaries.
-public class KvIbKit { }
+/// Various utilities for standard *Bundle* class.
+public class KvBundleKit { }
+
+
+
+// MARK: Info Dictionary
+
+extension KvBundleKit {
+
+    /// - Returns: Value for *CFBundleName* key from the info dictionary of given *bundle*.
+    public static func applicationName(_ bundle: Bundle = .main) -> String? {
+        bundle.object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+
+
+
+    /// - Returns: Value for *CFBundleShortVersionString* key from the info dictionary of given *bundle*.
+    public static func shortVersion(_ bundle: Bundle = .main) -> String? {
+        bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    }
+
+
+
+    /// - Returns: Value for *CFBundleVersion* key from the info dictionary of given *bundle*.
+    public static func bundleVection(_ bundle: Bundle = .main) -> String? {
+        bundle.object(forInfoDictionaryKey: "CFBundleVersion") as? String
+    }
+
+}
 
 
 
 // MARK: NIB
 
-extension KvIbKit {
+extension KvBundleKit {
 
     #if os(macOS)
     /// An instantiation method with error checking.
-    @available(*, deprecated, renamed: "KvBundleKit.viewFromNib(named:index:bundle:owner:)")
     @discardableResult
     public static func viewFromNib<T: NSView>(named nibName: String, index: Int? = nil, bundle: Bundle = .main, owner: Any? = nil) throws -> T {
         var topLevelObjects: NSArray?
@@ -52,7 +94,6 @@ extension KvIbKit {
 
     #if os(iOS)
     /// An instantiation method with error checking.
-    @available(*, deprecated, renamed: "KvBundleKit.viewFromNib(named:index:bundle:owner:)")
     @discardableResult
     public static func viewFromNib<T: UIView>(named nibName: String, index: Int? = nil, bundle: Bundle = .main, owner: Any? = nil) throws -> T {
         guard let topLevelObjects = bundle.loadNibNamed(nibName, owner: owner) else {
