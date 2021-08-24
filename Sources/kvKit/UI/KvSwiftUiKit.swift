fa4b388b4fe4c55442ef19a25f0b82d2e19b59fe
sdpopov <sdpopov@keyvar.com>
2021-08-24 18:43:31 +0300

    ErrorAlertContent

--- Sources/kvKit/UI/KvSwiftUiKit.swift
+++ Sources/kvKit/UI/KvSwiftUiKit.swift
@@ -39,21 +39,22 @@ public class KvSwiftUiKit { }
 extension KvSwiftUiKit {
 
     public static func alert(error: Error, dismissButton: Alert.Button? = nil) -> Alert {
-        let (title, message) = alertContent(for: error)
+        let content = ErrorAlertContent(for: error)
 
-        return Alert(title: title, message: message, dismissButton: dismissButton)
+        return Alert(title: content.titleText, message: content.messageText, dismissButton: dismissButton)
     }
 
 
 
     public static func alert(error: Error, primaryButton: Alert.Button, secondaryButton: Alert.Button) -> Alert {
-        let (title, message) = alertContent(for: error)
+        let content = ErrorAlertContent(for: error)
 
-        return Alert(title: title, message: message, primaryButton: primaryButton, secondaryButton: secondaryButton)
+        return Alert(title: content.titleText, message: content.messageText, primaryButton: primaryButton, secondaryButton: secondaryButton)
     }
 
 
 
+    @available(*, deprecated, message: "Use .ErrorAlertContent instead")
     public static func alertContent(for error: Error) -> (title: Text, message: Text?) {
         let message: Text? = {
             var message = KvStringKit.Accumulator(separator: "\n")
@@ -69,6 +70,66 @@ extension KvSwiftUiKit {
         }()
 
         return (title: Text(error.localizedDescription), message: message)
+    }
+
+
+
+    // MARK: .ErrorAlertContent
+
+    public struct ErrorAlertContent {
+
+        public var title: String
+        public var message: String?
+
+
+        @inlinable
+        public var titleText: Text { Text(title) }
+
+        @inlinable
+        public var messageText: Text? { message.map({ Text($0) }) }
+
+
+        @inlinable
+        public init(title: String, message: String? = nil) {
+            self.title = title
+            self.message = message
+        }
+
+
+        public init(for error: Error) {
+            title = error.localizedDescription
+            message = {
+                var message = KvStringKit.Accumulator(separator: Constants.separator)
+
+                if let error = error as? LocalizedError {
+                    message.append(error.failureReason ?? "")
+                    message.append(error.recoverySuggestion ?? "")
+                }
+
+                guard let string = message.string else { return nil }
+
+                return string
+            }()
+        }
+
+
+        // MARK: .Constants
+
+        public enum Constants {
+
+            public static let separator: String = "\n"
+
+        }
+
+
+        // MARK: Auxiliaries
+
+        /// - Returns: Joined title and message if available. If the result is empty then *nil* is returned.
+        @inlinable
+        public func joined() -> String? {
+            KvStringKit.Accumulator([ title, message ].compactMap({ $0 }), separator: Constants.separator).string
+        }
+
     }
 
 }
