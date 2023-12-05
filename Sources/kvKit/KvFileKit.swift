babb5777a2ea0964ab8061652be40cc880b51a36
Svyatoslav Popov <sdpopov@keyvar.com>
2023-12-06 00:02:37 +0300

    Convenient replacement method for file system items.

--- Sources/kvKit/KvFileKit.swift
+++ Sources/kvKit/KvFileKit.swift
@@ -25,7 +25,76 @@ import Foundation
 
 
 
-public class KvFileKit { }
+public struct KvFileKit { private init() { } }
+
+
+
+// MARK: File Manipilations
+
+extension KvFileKit {
+
+    /// Simple file item replacement procedure.
+    ///
+    /// First method removes the old backup item if exists.
+    /// Then It appends ".bak" extension to original item, moves replacement to original URL, then deletes or reverts the backup item whether replacements has successfully moved.
+    ///
+    /// - Throws: ``ReplaceError``.
+    ///
+    /// - Note: This method is suitable for directories.
+    public static func replaceItem(at originalURL: URL, withItemAt replacementURL: URL) throws {
+        let fileManager = FileManager.default
+
+        switch fileManager.fileExists(atPath: originalURL.path) {
+        case true:
+            let backupURL = originalURL.appendingPathExtension("bak")
+
+            if fileManager.fileExists(atPath: backupURL.path) {
+                do { try fileManager.removeItem(at: backupURL) }
+                catch { throw ReplaceError.unableToRemoveOldBackupItem(backupURL, error) }
+            }
+
+            do { try fileManager.moveItem(at: originalURL, to: backupURL) }
+            catch { throw ReplaceError.unableToMoveItem(at: originalURL, to: backupURL) }
+
+            do { try fileManager.moveItem(at: replacementURL, to: originalURL) }
+            catch {
+                // Revert the backup.
+                do { try fileManager.moveItem(at: backupURL, to: originalURL) }
+                catch { print("ERROR: Failed to revert the backup at `\(backupURL)` to original `\(originalURL)`") }
+
+                throw ReplaceError.unableToMoveItem(at: replacementURL, to: originalURL)
+            }
+
+        case false:
+            do { try fileManager.moveItem(at: replacementURL, to: originalURL) }
+            catch { throw ReplaceError.unableToMoveItem(at: replacementURL, to: originalURL) }
+        }
+    }
+
+
+    // MARK: .ReplaceError
+
+    /// Enumeration of errors thrown by ``replaceItem(at:withItemAt:)``.
+    public enum ReplaceError : LocalizedError {
+        
+        case unableToMoveItem(at: URL, to: URL)
+        case unableToRemoveOldBackupItem(URL, Error)
+
+
+        // MARK: : LocalizedError
+
+        public var errorDescription: String? {
+            switch self {
+            case .unableToMoveItem(at: let sourceURL, to: let destinationURL):
+                return "Failed to move item at `\(sourceURL)` to `\(destinationURL)`"
+            case .unableToRemoveOldBackupItem(let backupURL, let error):
+                return "Failed to remove old backup item at `\(backupURL)`. \(error)"
+            }
+        }
+
+    }
+
+}
 
 
 
